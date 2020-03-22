0e9253cd12c5411da20d92d9236692ef480a0dec
ggc99 <lgustavogarcial@gmail.com>
2020-03-22 11:47:48 -0500

    Added points being drawn

--- main.py
+++ main.py
@@ -1,15 +1,38 @@
 import sys
 import pyglet
 from math import *
+import random
 
-windowWidth = 800
-windowHeight = 800
+windowSize= 800
 
 #The length of one side of the square
 #in percentage of the window
 SIZE = 0.8
+WINDOW_CENTER = windowSize/2
+HALF_SIZE = WINDOW_CENTER*SIZE
+CIRCLE_RADIUS = (windowSize/2)*SIZE
 
-NUM_OF_VERTICES = 10000
+#Number of Vertices used to draw the circle
+NUM_OF_VERTICES = 50000
+
+#Speed at which points are drawn
+SPEED = 1
+
+allPoints = []
+
+class Point:
+    def __init__(self):
+        vertexFormat = 'v2i'
+        color = ('c3B', [100,500,300])
+
+        #create new point at random position
+        x = random.randint(abs(WINDOW_CENTER-HALF_SIZE), WINDOW_CENTER+HALF_SIZE)
+        y = random.randint(abs(WINDOW_CENTER-HALF_SIZE), WINDOW_CENTER+HALF_SIZE)
+
+        self.vertices = pyglet.graphics.vertex_list(1, (vertexFormat, [x,y]), color)
+    
+    def draw(self):
+        self.vertices.draw(pyglet.gl.GL_POINTS)
 
 class Circle:
     def __init__(self):
@@ -18,17 +41,16 @@ class Circle:
         color = [0,0,0]*(NUM_OF_VERTICES)
 
         #Center the circle in the square
-        centerX = windowWidth/2
-        centerY = windowHeight/2
+        center = windowSize/2
 
         #Radius based on size of length of a side of the square
-        size = centerX * SIZE
-
+        size = center * SIZE
+        
         verts = []
         for i in range(NUM_OF_VERTICES):
             angle = radians(float(i)/NUM_OF_VERTICES * 360.0)
-            x = size*cos(angle) + centerX
-            y = size*sin(angle) + centerX
+            x = size*cos(angle) + center
+            y = size*sin(angle) + center
             verts += [x,y]
         
         self.vertices = pyglet.graphics.vertex_list(NUM_OF_VERTICES, (vertexFormat, verts), (colorFormat, color))
@@ -52,13 +74,12 @@ class Square:
         self.vertices = pyglet.graphics.vertex_list(4, (vertexFormat, allVertices), (colorFormat, color))
 
     def get_vectors(self):
-        centerX = windowHeight/2        
-        centerY = windowWidth/2
-        halfSizeX = (SIZE*windowWidth)/2
-        halfSizeY = (SIZE*windowHeight)/2
+        centerX = windowSize/2        
+        centerY = windowSize/2
+        halfSizeX = (SIZE*windowSize)/2
+        halfSizeY = (SIZE*windowSize)/2
         
         return [centerX-halfSizeX,centerY+halfSizeY, centerX+halfSizeX,centerY+halfSizeY, centerX+halfSizeX,centerY-halfSizeY, centerX-halfSizeX,centerY-halfSizeY]
-
 
     def draw(self):
         self.vertices.draw(pyglet.gl.GL_QUADS)
@@ -73,13 +94,19 @@ class View(pyglet.window.Window):
         self.clear()
         self.square.draw()
         self.circle.draw()
+        for point in allPoints:
+            point.draw()
+    
+    def update(self, dt):
+        for i in range(SPEED):
+            allPoints.append(Point())
 
 if __name__ == '__main__':
     #Create window
-    window = View(width=windowWidth, height=windowHeight, caption="Estimating Pi", resizable=False)
+    window = View(width=windowSize, height=windowSize, caption="Estimating Pi", resizable=False)
 
     #Set Background
     pyglet.gl.glClearColor(1,1,1,0.5)
 
-    #pyglet.clock.schedule_interval(window.update, 1/10.0)
+    pyglet.clock.schedule_interval(window.update, 1/30.0)
     pyglet.app.run()
